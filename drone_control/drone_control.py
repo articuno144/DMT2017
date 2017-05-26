2c87b8e0897e856d5c2a43badf13df20ceb0358c
xinyangyuan <xinyangyuan@hotmail.com>
2017-05-26 14:46:52 +0100

    pid control

--- drone_control/drone_control.py
+++ drone_control/drone_control.py
@@ -11,6 +11,21 @@ from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
 
 
+def get_loc(vc1, ff1, vc2, ff2, vc3, ff3, a, b, c, loc_prev, vel_prev):
+    """ 
+    Return a weighted sum of location from the camera
+    and the previous momentum.
+    """
+    x1, y1 = cam.frame_loc(vc1, ff1, 0)
+    x2, y2 = cam.frame_loc(vc2, ff2, 0)
+    x3, y3 = cam.frame_loc(vc3, ff3, 0)
+    t1, t2, t3 = cam.get_angle(x1, y1), cam.get_angle(
+        x2, y2), cam.get_angle(x3, y3)
+    cam_coord = cam.get_coordinates(t1, t2, t3, a, b, c)
+    loc_current = 0.7*cam_coord+0.3*(loc_prev+vel_prev)
+    return loc_current, (loc_current-loc_prev)
+
+
 class Drone():
     """
     Here we are only using the cameras and the onboard
@@ -29,9 +44,6 @@ class Drone():
         self._cf = Crazyflie()
         self.uri = link_uri
         self.target = np.array([0, 0, 0])
-        self.loc_prev = None
-        self.vel_prev = None
-        self.not_found_counter = 0
 
     def Initialise(self):
         """ 
@@ -45,57 +57,49 @@ class Drone():
         self.cmd.send_setpoint(0, 0, 0, 0)
         return self.cmd
 
-    def Go_to(self, target, Commander, Kp=1, Ki=0, Kd=0):
+    def Go_to(self, target, Kp, Ki, Kd):
         """ PID controller to get to specific location """
-        if self.not_found_counter > 10:
-            # drone lost
-            Commander.send_setpoint(0, 0, 0, 0)
         #################################
+        while 1:
+        loc= self.loc
+        thrust=PID_controller(loc(1),target(1),1000,100,10)
+        #roll=PID_controller(loc(2),tar(2),1000,100,10)
+        #pitch=PID_controller(loc(3),tar(3),1000,100,10)
+        send_setpoint(0,0,0,thrust)
         return
 
-    def get_loc(self, coordinates, read_failed, loc_prev=None, vel_prev=None):
-        """ 
-        Return a weighted sum of location from the camera
-        and the previous momentum.
-        """
-        cam_coord = np.array(coordinates)
-        if loc_prev == None:
-            return cam_coord, np.array([0, 0, 0])
-        if read_failed[1]:
-            self.not_found_counter += 1
-            return loc_prev+vel_prev
-        self.not_found_counter = 0
-        loc_current = 0.7*cam_coord+0.3*(loc_prev+vel_prev)
-        return loc_current, (loc_current-loc_prev)
+    def PID_controller(y,yt,h=0.01,Kp,Ti,Td,e0=0,ei0=0)
+    # Step variable
+    k = 0
+
+    # Initialization
+    ei_prev = ei0
+    e_prev = e0
+
+    while 1:
+
+        # Error between the desired and actual output
+        e = yt - y
+
+        # Integration Input
+        ui = ei_prev + 1/Ti * h*e
+        # Derivation Input
+        ud = 1/Td * (e - e_prev)/h
+
+        # Adjust previous values
+        e_prev = e
+        ui_prev = ui
+
+        # Calculate input for the system
+        u = Kp * (e + ui + ud)
+        
+        k += 1
+
+        yield u
 
 
-def control(target, link_uri):
-    """
-    The main control function, to be called as a separate thread from the gesture
-    recognition part. This function continuously reads the targets and attemps to
-    move the drone to the target.
-    """
-    if type(link_uri) == str:
-        coordinates = [0, 0, 0]
-        read_failed = [0]
-        # Initialise
-        vc0, vc1, vc2, first_frame0, first_frame1, first_frame2 = cam.Init()
-        cf = Drone("radio://0/80/250K")
-        cmd = cf.Initialise()
-        while True:
-            # updates the coordinate list from the camera feed
-            cam.Cam(coordinates, read_failed, vc0, vc1, vc2,
-                    first_frame0, first_frame1, first_frame2)
-            key = cv2.waitKey(10)
-            if key == 27:  # exit on ESC
-                cv2.VideoCapture(1).release()
-                cv2.VideoCapture(2).release()
-                cv2.VideoCapture(3).release()
-                break
-            # updates the drone location and velocity
-            cf.loc, cf.vel = cf.get_loc(
-                coordinates, read_failed, cf.loc, cf.vel)
-            cf.Go_to(target, cmd)
-    if type(link_uri) == list:
-        assert len(target) == len(
-            link_uri), "Provide exactly one link_uri for each target location"
+    def Update(self):
+        """ Updates the drone location and velocity """
+        loc_prev, vel_prev = self.loc, self.vel
+        self.loc = get_loc(loc_prev, vel_prev)
+        self.vel = (self.loc - loc_prev)
